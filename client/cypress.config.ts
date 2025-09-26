be29f5f89d62a55369923658df03dc4f78381477
OEMromanca <r.jamrisko@gmail.com>
2025-09-26 20:34:54 +0200

    config

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -9,8 +9,8 @@ export default defineConfig({
   },
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:5000",
+    supportFile: false,
+    setupNodeEvents(_, config) {},
   },
 });
