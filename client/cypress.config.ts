dfd6a6f5ddb065fb7b5513297912bbfc019bc794
OEMromanca <r.jamrisko@gmail.com>
2025-09-26 21:51:15 +0200

    pipeline fixes

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -9,7 +9,9 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: "http://localhost:5000",
+    baseUrl: "http://localhost:5173",
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+
     supportFile: false,
     setupNodeEvents(_, config) {},
   },
