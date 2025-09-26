8fc77711ae05899569947d280dab662b7a488763
OEMromanca <r.jamrisko@gmail.com>
2025-09-26 11:07:46 +0200

    minor changes

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,4 @@
-/// <reference types="vite/client" />
+declare module "pdfmake/interfaces";
+declare module "pdfmake/build/vfs_fonts";
+declare module "pdfmake/build/pdfmake";
+declare module "react-to-pdf";
