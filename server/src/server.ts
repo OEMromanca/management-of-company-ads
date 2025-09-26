c622fa9214718a8fd41e0603c8599d4562d3fbad
OEMromanca <r.jamrisko@gmail.com>
2025-09-26 14:32:04 +0200

    pipeline fixes

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,6 +17,8 @@ app.use(
   })
 );
 
+console.log("Connecting to MongoDB with URI:", process.env.MONGO_URI);
+
 mongoose
   .connect(process.env.MONGO_URI!)
   .then(() => console.log("✅ MongoDB pripojená"))
